171157a758b98721119e29353c198b30dec99751
Igor <iigorfelipe@gmail.com>
2022-06-21 15:59:14 -0300

    praticando typescript e nodejs

---
+++ src/schemas/User.ts
@@ -0,0 +1,17 @@
+import { Schema, model, Document } from 'mongoose'
+
+interface IUser extends Document {
+  email?: string
+  firstName?: string
+  lastName?: string
+}
+
+const UserSchema = new Schema({
+  email: String,
+  firstName: String,
+  lastName: String
+}, {
+  timestamps: true
+})
+
+export default model<IUser>('User', UserSchema)
